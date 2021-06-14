7fee20e38401b5fa385c2da7645bf660221603a4
rie腐竹 <1050740155@qq.com>
2021-06-14 20:13:03 +0800

    update main.cpp.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include "gamewidget.h"
 #include <QApplication>
 
+#include "client.h"
+Client *client;
+
 int main(int argc, char *argv[])
 {
+  client = new Client();
     QApplication a(argc, argv);
     StartPage w;
     //GameWidget w;
